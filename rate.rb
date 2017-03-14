3486da6da72e74b81a80501aee59bf68fdea7fd1
Kathy Toufighi <kathy.toufighi@gmail.com>
2017-03-14 17:34:12 -0400

    creatd a new method to produce product description and print name and total in the terminal

--- rate.rb
+++ rate.rb
@@ -1,7 +1,8 @@
 class Rate
 
-    def initialize(type, price, num_worker)
+    def initialize(name, type, price, num_worker)
 
+    @name = name
     @type = type
     @price = price
     @num_worker = num_worker
@@ -38,14 +39,40 @@ class Rate
   def total_quote
     @price + markups + worker_rate
   end
+
+  def product_description
+    @name + total_quote.to_s
+  end
+
 end
 
+
+
 # Input
-  food = Rate.new('food', 30, 3)
-  medication = Rate.new('medication', 12.45, 1)
-  basic = Rate.new('basic', 20.99, 1)
+  food = Rate.new('food', 'food', 30, 3)
+  medication = Rate.new('medication', 'medication', 12.45, 1)
+  basic = Rate.new('basic', 'basic', 20.99, 1)
 
 # Final number
 puts "Output1: Snacks $#{food.total_quote}"
 puts "Output2: Tylenol $#{medication.total_quote}"
 puts "Output3: Lord of the Rings $#{basic.total_quote}"
+
+
+puts ""
+puts "FIRST ITEM"
+# input
+steak = Rate.new("Steak and Cheese: $", 'food', 30, 2)
+# output
+puts steak.product_description
+puts "Workers markup: $#{steak.worker_rate}"
+puts "Product markup: $#{steak.markups}"
+
+puts ""
+puts "NEXT ITEM"
+# input
+book = Rate.new("Harry Potter: $", 'food', 14.99, 1)
+# output
+puts book.product_description
+puts "Workers markup: $#{book.worker_rate}"
+puts "Product markup: $#{book.markups}"
